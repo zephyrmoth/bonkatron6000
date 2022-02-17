4dc2ce2183074774e26b2ece16954c3cd538d92f
zephyrmoth <65647019+zephyrmoth@users.noreply.github.com>
2022-02-17 07:27:11 -0600

    Load user list from external file and improved error handling

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Install
 Depends on the following Python3 packages:
 discord.py
-Place the Discord bot token in a file in the secrets directory named discord.token.
-Invite to server from admin interface.
+Place the Discord bot token in a file named discord.token.
+Invite to server from developer interface.
 
 Running
 Run bot.py
--- bot.py
+++ bot.py
@@ -1,38 +1,54 @@
 import discord
+import json
+import sys
 
-#TODO: move to external conf file
-#TODO: set up proper logging
-#custom emoji keys are obtainable by escaping emoji
-TEMP_BONKABLE_LIST = {269310283439144961:{"strings":["bonk me"], "react":"💯"},458362561130397707:{"strings":["uwu", "UwU", "owo", "OwO"], "react":915287209077248030}}
+# TODO: set up proper logging
+# Custom emoji keys are obtainable by escaping emoji
+# Yes the error messages are intentionally cursed
 class BonkatronClient(discord.Client):
+    # Init and load bonkable list
+    def __init__(self, bonkable_list):
+        self.bonkable_list = bonkable_list
+        super().__init__()
+
+    # Error checking on boot
     async def on_ready(self):
         print("Hewwo Wowld :3! It's me {}".format(self.user))
+        print(self.bonkable_list)
     
     #Check if user is in "bonkable" list
     async def on_message(self, message):
-        print("Message received {} :3".format(message.author.id))
-        if message.author.id in TEMP_BONKABLE_LIST.keys():
-            print("Bonkable user :3")
-            user_id = message.author.id
+        if str(message.author.id) in self.bonkable_list.keys():
+            user_id = str(message.author.id)
             bonkable = False
             #This is almost certainly not super efficient but this is entirely a goof
-            for string in TEMP_BONKABLE_LIST[user_id]["strings"]:
+            for string in self.bonkable_list[user_id]["strings"]:
                 if string in message.content:
                     bonkable = True
                     break
             if bonkable:
-                print("Bonk :3".format(self.user))
                 emoji = None
-                if isinstance(TEMP_BONKABLE_LIST[user_id]["react"], str):
-                    emoji = TEMP_BONKABLE_LIST[user_id]["react"]
-                elif isinstance(TEMP_BONKABLE_LIST[user_id]["react"], int):
-                    emoji = self.get_emoji(TEMP_BONKABLE_LIST[user_id]["react"])
+                if isinstance(self.bonkable_list[user_id]["react"], str):
+                    emoji = self.bonkable_list[user_id]["react"]
+                elif isinstance(self.bonkable_list[user_id]["react"], int):
+                    emoji = self.get_emoji(self.bonkable_list[user_id]["react"])
+                    if not emoji:
+                        print("Oh noes, invalid emoji ;-;")
+                        return
                 else:
-                    print("Oh noes, invalid emoji ;-;")
+                    print("Oh noes, invalid emoji format ;-;")
                     return
+                print("Bonked {} :3".format(message.author.display_name))
                 await message.add_reaction(emoji)
+    
+def get_bonk_list():
+    bonk_file = open("./bonkable.json", "r")
+    bonkable_list = json.loads(bonk_file.read().strip())
+    return bonkable_list
 
 if __name__ == '__main__':
+    # Load external conf file
+    bonkable_list = get_bonk_list()
     #Read client secret from client.secret
     try:
         discord_keyfile = open("./discord.token", "r")
@@ -40,5 +56,5 @@ if __name__ == '__main__':
     except:
         print("Could not find Discord bot token! Exiting...")
         sys.exit(1)
-    client = BonkatronClient()
+    client = BonkatronClient(bonkable_list)
     client.run(DISCORD_TOKEN)
